b3b1e0f793ad09f0d8959669ec7105cc63ab92c3
a.beylann@gmail.com <alix_1995@live.com.mx>
2015-12-13 06:25:44 +0000

    Get planet's description by setting up the size using as an input the top-rigt coordinates.

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,25 @@ public class Main {
         Planet planet = null;
         ArrayList<Robot> robots = new ArrayList<Robot>();
 
-        // get planet details
-        String rawPlanetSize;
+        // Get planet description.
+        String planetSize;
         System.out.println("Let's make a planet!");
+
+        // Set up planet's size by using the top right coordinates.
+        while (planet == null) {
+            System.out.print("\nPlease enter the planet's top-right coordinate x y : ");
+            planetSize = s.nextLine();
+            // check format of user input
+            if (planetSize.matches("[0-9]+\\s+[0-9]+")) {
+                String[] coordinateParts = planetSize.split("\\s+");
+                planet = new Planet(Integer.parseInt(coordinateParts[0]), Integer.parseInt(coordinateParts[1]));
+            } else {
+                // loop to try again
+                System.err.println("\nSomething went wrong... Please enter your planet size in the format x y");
+                System.err.flush();
+            }
+        }
+
 
     }
 }
